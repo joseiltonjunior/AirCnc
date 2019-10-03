71f10462eed0b677a2ea81f4c6a5cc63e8729002
joseiltonjunior <joseiltonjuniortech@gmail.com>
2019-10-02 21:02:04 -0300

    tudo de novo :(

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
-const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@oministack-kgcvl.mongodb.net/omnistack?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-
-//GET, POST, PUT, DELETE;
-
-// req.query = Acessar query params (para filtros);
-// req.params = Acessar route params (para edição e delete);
-// req.body = Acessar corpo da requisição (para criação e edição);
-
-app.use(express.json());
-app.use(routes);
+app.get('/', (req, res) => {
+    return res.json({ message: "Olá JR" });
+});
 
 app.listen(3333);
