6171dccecd665e387d3e109f37f62887b5dbfb14
joseiltonjunior <joseiltonjuniortech@gmail.com>
2019-10-02 22:02:32 -0300

    por hoje é só.
    continua...

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: "Olá JR" });
+mongoose.connect('mongodb+srv://omnistack:omnistack@oministack-kgcvl.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
+
+// req.query = acessa o query params
+// req.params = acessa o route params
+// req.body = acessa o corpo da requisição
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
