70619011312efc8adc27db1cf9696f164d39496c
joseiltonjunior <joseiltonjuniortech@gmail.com>
2019-10-21 21:41:14 -0300

    continuação
    continuaa...

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { View, KeyboardAvoidingView,Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, KeyboardAvoidingView, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [techs, setTechs] = useState('');
+
+    async function handleSubmit() {
+        console.log(email);
+        console.log(techs);
+    }
+
     return (
         <KeyboardAvoidingView behavior='padding' style={styles.container}>
             <Image source={logo} />
@@ -17,6 +27,8 @@ export default function Login() {
                     keyboardType="email-address"
                     autoCapitalize="none"
                     autoCorrect={false}
+                    value={email}
+                    onChangeText={setEmail}
                 />
 
                 <Text style={styles.label}>TECNOLOGIAS *</Text>
@@ -26,9 +38,11 @@ export default function Login() {
                     placeholderTextColor="#999"
                     autoCapitalize="words"
                     autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
                 />
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity onPress={handleSubmit} style={styles.button}>
                     <Text style={styles.buttonText}>Encontrar Spots</Text>
                 </TouchableOpacity>
             </View>
